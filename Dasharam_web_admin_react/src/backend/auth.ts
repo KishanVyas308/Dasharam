d1367e5d6f5624626fa8872a2fdcdbb35b0e036d
Kishan Vyas <kishanvyas308@gmail.com>
2024-10-13 22:56:48 +0530

    correct logic for login

--- Dasharam_web_admin_react/src/backend/auth.ts
+++ Dasharam_web_admin_react/src/backend/auth.ts
@@ -1,16 +1,22 @@
-import { collection, getDoc, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "./firebase";
+
 
 export const login = async (name: string, password: string) => {
   try {
-    const q: any = query(collection(db, "admin"), where("name", "==", name));
-    const admin: any = await getDoc(q);
-    if (admin.exists()) {
-      if (admin.data().password === password) {
-        return admin.data();
+    const q = query(collection(db, "Admin"), where("name", "==", name));
+    const querySnapshot = await getDocs(q);
+    
+    if (!querySnapshot.empty) {
+      const adminDoc = querySnapshot.docs[0]; // Assuming there's only one admin with this name
+      const adminData = adminDoc.data();
+      
+      if (adminData.password === password) {
+        return adminData;
       }
     }
   } catch (error) {
+    console.log(error);
     return error;
   }
 
