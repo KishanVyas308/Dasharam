431f6ebd50426d233ee841e9557882510a5fe5c2
Kishan Vyas <kishanvyas308@gmail.com>
2024-10-17 15:26:00 +0530

    added teacher login

--- Dasharam_web_admin_react/src/backend/auth.ts
+++ Dasharam_web_admin_react/src/backend/auth.ts
@@ -8,11 +8,23 @@ export const login = async (name: string, password: string) => {
     const querySnapshot = await getDocs(q);
     
     if (!querySnapshot.empty) {
-      const adminDoc = querySnapshot.docs[0]; // Assuming there's only one admin with this name
+      const adminDoc = querySnapshot.docs[0]; // there's only one admin with this name
       const adminData = adminDoc.data();
       
       if (adminData.password === password) {
         return adminData;
+      }
+    }
+
+    const q1 = query(collection(db, "teachers"), where("grNo", "==", name));
+    const querySnapshot1 = await getDocs(q1);
+
+    if (!querySnapshot1.empty) {
+      const teacherDoc = querySnapshot1.docs[0]; // there's only one teacher with this name
+      const teacherData = teacherDoc.data();
+      
+      if (teacherData.password === password) {
+        return teacherData;
       }
     }
   } catch (error) {
